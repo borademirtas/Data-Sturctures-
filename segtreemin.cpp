4110e5219d25a2bd9b068bd52397a8487f4390cc
Bora <borademirtas@yahoo.com>
2022-11-16 01:33:27 -0600

    Create segtreemin.cpp

---
+++ segtreemin.cpp
@@ -0,0 +1,31 @@
+template<class T>
+class mnseg {
+private:
+    T comb(T a, T b) { return std::min(a, b); }
+    const T DEFAULT = 1e18; 
+    vector<T> segtree;
+    int len;
+public:
+    mnseg(int len) : len(len), segtree(len * 2, DEFAULT) { }
+    void set(int ind, T val) {
+        assert(0 <= ind && ind < len);
+        ind += len;
+        segtree[ind] = val;
+        for (; ind > 1; ind /= 2) {
+            segtree[ind >> 1] = comb(segtree[ind], segtree[ind ^ 1]);
+        }
+    }
+    T mn(int start, int end) {
+        assert(0 <= start && start < len && 0 < end && end <= len);
+        T sum = DEFAULT;
+        for (start += len, end += len; start < end; start /= 2, end /= 2) {
+            if ((start & 1) != 0) {
+                sum = comb(sum, segtree[start++]);
+            }
+            if ((end & 1) != 0) {
+                sum = comb(sum, segtree[--end]);
+            }
+        }
+        return sum;
+    }
+};
