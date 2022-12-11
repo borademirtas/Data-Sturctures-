340ef94f0534df30a3d6667e0b1dd23b593f76da
Bora <borademirtas@yahoo.com>
2022-12-11 17:12:27 -0600

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,14 @@
+struct DSU {
+	vector<int> e;
+	DSU(int N) { e = vector<int>(N, -1); }
+	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+	bool same_set(int a, int b) { return get(a) == get(b); }
+	int size(int x) { return -e[get(x)]; }
+	bool unite(int x, int y) { e
+		x = get(x), y = get(y);
+		if (x == y) return false;
+		if (e[x] > e[y]) swap(x, y);
+		e[x] += e[y]; e[y] = x;
+		return true;
+	}
+};
