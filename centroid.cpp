21ea85d69ccd2dba1cfe227e3c4a3e495597d858
Bora <borademirtas@yahoo.com>
2023-08-04 11:35:49 -0500

    Update centroid.cpp

--- centroid.cpp
+++ centroid.cpp
@@ -1,7 +1,7 @@
 struct centroid{
     vector<set<ll>> adj; vll sub, par; ll nn;
     void init(ll n){adj.resize(n+1); sub.resize(n+1); par.resize(n+1);}
-    void dfs1(ll u, ll p){nn++; for(ll v: adj[u]){if(v!=p){dfs1(v,u); sub[u]+=sub[v];}}}
+    void dfs1(ll u, ll p){nn++; sub[u] = 1; for(ll v: adj[u]){if(v!=p){dfs1(v,u); sub[u]+=sub[v];}}}
     ll dfs2(ll u, ll p){for(ll v: adj[u]){if(v!=p && sub[v]>nn/2){return dfs2(v,u);}} return u;}
     void decomp(ll root, ll p){
         nn = 0;
