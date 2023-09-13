df88ecca672e4303d4c559f6f8aa2d887e3827a3
Bora <borademirtas@yahoo.com>
2023-09-13 05:30:27 -0500

    Create pdsu.cpp

---
+++ pdsu.cpp
@@ -0,0 +1,21 @@
+struct DSU {
+    vll time, par; vector<vll> version, sz;
+    DSU(int N) {
+        time.resize(N); par.resize(N); version.resize(N); sz.resize(N);
+        for(ll i = 0; i < N; i++){par[i]=i; version[i].pb(0); sz[i].pb(0);}
+    }
+    int get(int x, int t) {
+         if(par[x]==x||time[x]>t){return x;}
+         else{return get(par[x], t);}
+    }
+    int size(int x, int t) {x = get(x,t); int pos = upper_bound(version[x].begin(),version[x].end(),t) - version[x].begin(); return sz[x][pos-1]; }
+    void unite(int x, int y, int t ) {
+        x = get(x,t); y = get(y,t);
+        if(x==y){return;}
+        if(sz[x]>sz[y]){swap(x,y);}
+        par[x]=y;
+        time[x]=t;
+        version[y].pb(t);
+        sz[y].pb(sz[x].back()+sz[y].back());
+    }
+};
